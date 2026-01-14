61637958af388a798077ec9521914b2c83815c69
isu <natantrojak@wp.pl>
2026-01-15 00:00:45 +0100

    Modified snacks.nvim dashboard config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,6 +3,26 @@ return {
     'folke/snacks.nvim',
     priority = 1000,
     opts = {
+      dashboard = {
+        enabled = true,
+        preset = {
+          header = [[
+███╗   ██╗████████╗██████╗
+████╗  ██║╚══██╔══╝██╔══██╗
+██╔██╗ ██║   ██║   ██████╔╝
+██║╚██╗██║   ██║   ██╔══██╗
+██║ ╚████║   ██║   ██║  ██║
+╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝
+                      ]],
+        },
+        sections = {
+          { section = 'header' },
+          { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
+          { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
+          { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
+          { section = 'startup' },
+        },
+      },
       lazygit = {
         enabled = true,
       },
